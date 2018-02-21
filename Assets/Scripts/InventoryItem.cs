8f579037e6d519b100da7c6e655cb64a2161a5d9
skumer <s.kumer@outlook.de>
2018-02-21 20:23:03 +0100

    5

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : ScriptableObject
+{
+
+    public string itemName;
+    public Sprite sprite;
+    public AudioClip picSound;
+    public GameObject prefab;
+}
---
+++ Assets/Scripts/PickalbleItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableItem : MonoBehaviour
+{
+
+    public InventoryItem inventoryItem;
+    private Inventory inventory;
+    private Transform player;
+
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        inventory = player.GetComponent<Inventory>();
+
+    }
+
+    void OnMouseDown()
+    {
+        if (inventory.AddItem(inventoryItem))
+        {
+            if (inventoryItem.picSound != null)
+                AudioSource.PlayClipAtPoint(inventoryItem.picSound, player.position);
+
+            Destroy(gameObject);
+        }
+    }
+}
